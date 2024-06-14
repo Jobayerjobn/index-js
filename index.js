903a3fff587b0b76536efd1d7e4e11edaf720dc7
jobayerjobn <jobayerjoban0048@gamil.com>
2024-06-15 03:45:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('hello Jobayer');
+const name = "Jobayer";
+
+function myName(){
+    console.log('Hello ' + name + 'how are you? ')
+}
+myName();
+
+function add (x, y){
+    return x + y;
+};
+const result = add(2, 7);
+console.log(result);
